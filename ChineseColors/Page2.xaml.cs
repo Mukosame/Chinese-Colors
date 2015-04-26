847ad1511746723ac2219b902ded925a237d6b5c
Mukosame <mukosame@gmail.com>
2015-04-26 12:39:00 +0800

    v1.1.0.7

--- ChineseColors/Page2.xaml.cs
+++ ChineseColors/Page2.xaml.cs
@@ -9,9 +9,7 @@ using Windows.ApplicationModel.Resources;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
 using Windows.Graphics.Display;
-//using Windows.Storage.Pickers;
 using Windows.Storage;
-//using Windows.Graphics.Display;
 using Windows.Graphics.Imaging;
 using System.Text;
 using Windows.UI.Core;
@@ -30,45 +28,39 @@ using Windows.UI.Xaml.Media.Animation;//coloranimation
 using Windows.Data.Json;
 using Windows.UI.Popups;
 
-
-// The Hub Application template is documented at http://go.microsoft.com/fwlink/?LinkId=391641
+// The Basic Page item template is documented at http://go.microsoft.com/fwlink/?LinkID=390556
 
 namespace ChineseColors
 {
     /// <summary>
-    /// A page that displays a grouped collection of items.
+    /// An empty page that can be used on its own or navigated to within a Frame.
     /// </summary>
-    public sealed partial class HubPage : Page
+    public sealed partial class Page2 : Page
     {
-        private readonly NavigationHelper navigationHelper;
+        private NavigationHelper navigationHelper;
         private readonly ObservableDictionary defaultViewModel = new ObservableDictionary();
         private readonly ResourceLoader resourceLoader = ResourceLoader.GetForCurrentView("Resources");
         private ApplicationDataContainer localSettings;
+        int flag = 0;
         String appversion = GetAppVersion();
-        int flag=0;
 
-        public HubPage()
+        public Page2()
         {
             this.InitializeComponent();
             InitRandom();
-            localSettings = ApplicationData.Current.LocalSettings;
-            Windows.Phone.UI.Input.HardwareButtons.BackPressed += HardwareButtons_BackPressed;
-        
-            // Hub is only supported in Portrait orientation
-            DisplayInformation.AutoRotationPreferences = DisplayOrientations.Portrait;
-
-            this.NavigationCacheMode = NavigationCacheMode.Required;
-
             this.navigationHelper = new NavigationHelper(this);
             this.navigationHelper.LoadState += this.NavigationHelper_LoadState;
             this.navigationHelper.SaveState += this.NavigationHelper_SaveState;
+            localSettings = ApplicationData.Current.LocalSettings;
+            Windows.Phone.UI.Input.HardwareButtons.BackPressed += HardwareButtons_BackPressed;
         }
 
         private void HardwareButtons_BackPressed(object sender, Windows.Phone.UI.Input.BackPressedEventArgs e)
         {
-            if (!e.Handled && Frame.CurrentSourcePageType.FullName == "ChineseColors.HubPage")
+            if (!e.Handled && Frame.CurrentSourcePageType.FullName == "ChineseColors.Page2")
                 Application.Current.Exit();
         }
+
         /// <summary>
         /// Gets the <see cref="NavigationHelper"/> associated with this <see cref="Page"/>.
         /// </summary>
@@ -99,7 +91,6 @@ namespace ChineseColors
         /// session.  The state will be null the first time a page is visited.</param>
         private async void NavigationHelper_LoadState(object sender, LoadStateEventArgs e)
         {
-            // TODO: Create an appropriate data model for your problem domain to replace the sample data
             var sampleDataGroups = await SampleDataSource.GetGroupsAsync();
             this.DefaultViewModel["Groups"] = sampleDataGroups;
         }
@@ -114,21 +105,11 @@ namespace ChineseColors
         /// serializable state.</param>
         private void NavigationHelper_SaveState(object sender, SaveStateEventArgs e)
         {
-            // TODO: Save the unique state of the page here.
         }
 
-        /// <summary>
-        /// Shows the details of a clicked group in the <see cref="SectionPage"/>.
-        /// </summary>
         private void GroupSection_ItemClick(object sender, ItemClickEventArgs e)
         {
             var groupId = ((SampleDataGroup)e.ClickedItem).UniqueId;
-           /*
-            if (!Frame.Navigate(typeof(SectionPage), groupId))
-            {
-                throw new Exception(this.resourceLoader.GetString("NavigationFailedExceptionMessage"));
-            }
-            * */
         }
 
         /// <summary>
@@ -150,9 +131,9 @@ namespace ChineseColors
             var itemColor = ((SampleDataItem)e.ClickedItem).Color;
             Title.Text = ((SampleDataItem)e.ClickedItem).Title;
             RGB.Text = ((SampleDataItem)e.ClickedItem).Subtitle;
-            CMYK.Text = ((SampleDataItem)e.ClickedItem).Content;
-            HEX.Text = "HEX:"+ ((SampleDataItem)e.ClickedItem).Color.ToUpper();
-            des.Text =((SampleDataItem)e.ClickedItem).Description;
+            CMYK.Text = "CMYK:" + ((SampleDataItem)e.ClickedItem).Content;
+            pinyin.Text = ((SampleDataItem)e.ClickedItem).UniqueId.ToUpper();
+            HEX.Text = "HEX:" + ((SampleDataItem)e.ClickedItem).Color.ToUpper();
 
             changeColorAnimation.To = Color.FromArgb(255,
                 Convert.ToByte(itemColor.Substring(1, 2), 16),
@@ -169,20 +150,22 @@ namespace ChineseColors
              */
         }
 
+
         #region NavigationHelper registration
 
         /// <summary>
         /// The methods provided in this section are simply used to allow
         /// NavigationHelper to respond to the page's navigation methods.
         /// <para>
-        /// Page specific logic should be placed in event handlers for the
+        /// Page specific logic should be placed in event handlers for the  
         /// <see cref="NavigationHelper.LoadState"/>
         /// and <see cref="NavigationHelper.SaveState"/>.
-        /// The navigation parameter is available in the LoadState method
+        /// The navigation parameter is available in the LoadState method 
         /// in addition to page state preserved during an earlier session.
         /// </para>
         /// </summary>
-        /// <param name="e">Event data that describes how this page was reached.</param>
+        /// <param name="e">Provides data for navigation methods and event
+        /// handlers that cannot cancel the navigation request.</param>
         protected override void OnNavigatedTo(NavigationEventArgs e)
         {
             this.navigationHelper.OnNavigatedTo(e);
@@ -195,9 +178,8 @@ namespace ChineseColors
 
         #endregion
 
-#region APPBARBUTTON
+        #region APPBARBUTTON
         public static string GetAppVersion()
-        //using Windows.ApplicationModel;
         {
             Package package = Package.Current;
             PackageId packageId = package.Id;
@@ -209,8 +191,8 @@ namespace ChineseColors
         private void Spectrum(object sender, RoutedEventArgs e)
         {
             //设置绑定的数据集
-            Frame.Navigate(typeof(Page2));
-            if (!Frame.Navigate(typeof(Page2)))
+            Frame.Navigate(typeof(HubPage));
+            if (!Frame.Navigate(typeof(HubPage)))
             {
                 throw new Exception(this.resourceLoader.GetString("NavigationFailedExceptionMessage"));
             }
@@ -235,8 +217,7 @@ namespace ChineseColors
             RGB.Foreground = new SolidColorBrush(FontColor[flag]);
             CMYK.Foreground = new SolidColorBrush(FontColor[flag]);
             HEX.Foreground = new SolidColorBrush(FontColor[flag]);
-            des.Foreground = new SolidColorBrush(FontColor[flag]);
-
+            pinyin.Foreground = new SolidColorBrush(FontColor[flag]);
         }
 
         static Color[] FontColor = { Colors.White, Colors.LightGray};
@@ -251,20 +232,10 @@ namespace ChineseColors
             var pixelBuffer = await bitmap.GetPixelsAsync();
 
             string filename = Title.Text+".png";
-            //IStorageFolder applicationFolder = ApplicationData.Current.LocalFolder;
-            //IStorageFile saveFile = await applicationFolder.CreateFileAsync(filename, CreationCollisionOption.OpenIfExists);
             StorageFolder storageFolder = KnownFolders.PicturesLibrary;
             var saveFile = await storageFolder.CreateFileAsync(filename,
                 CreationCollisionOption.ReplaceExisting);
-            /*
-            using (Stream stream = await saveFile.OpenStreamForWriteAsync())
-            {
-                byte[] content = Encoding.UTF8.GetBytes("1");
-                await stream.WriteAsync(content, 0, content.Length);
-            }
-            await new MessageDialog("writing done").ShowAsync();
-            */
-            //save now as picture
+
             using (var fileStream = await saveFile.OpenAsync(FileAccessMode.ReadWrite))
             {
                 var encoder = await BitmapEncoder.CreateAsync(BitmapEncoder.PngEncoderId, fileStream);
@@ -302,64 +273,19 @@ namespace ChineseColors
 #endregion
         #region InitFeature
         //not finished
-        private async void InitRandom()
+        private void InitRandom()
         {
-                         /*
-            Title.Text = "粉红";
-            RGB.Text = "RGB:255,179,167";
-            CMYK.Text = "CMYK:0,37,26,0";
-            des.Text = "粉红，即浅红色。别称：妃色,杨妃色,湘妃色,妃红色";
-            Brush.Color = Color.FromArgb(255, 255, 179, 167);
-            Hub.Background = Brush;
-             */
             var Brush = new SolidColorBrush();
 
-            Random r = new Random();
-            int randomObjectIndext = r.Next(171);
-            var res = await SampleDataSource.GetGroupAsync("Group-1");
-//            SampleDataGroup res = new SampleDataGroup(null,null,null,null,null);
-            /*
-            if (randomObjectIndext > 171)
-            {
-                randomObjectIndext = randomObjectIndext - 171;
-                res = await SampleDataSource.GetGroupAsync("Group-2");
-            }
-            else
-            {                ;            }
-             * */
-            try
-            {
-                var item = res.Items.ElementAtOrDefault(randomObjectIndext);
-                //int Size = trends.Count;
-                //var item = trend;
-                //Title.Text = item.ToString();
-                Title.Text = item.Title;
-                RGB.Text = item.Subtitle;
-                CMYK.Text = item.Content; 
-                des.Text = item.Description;
-                HEX.Text = "HEX:"+item.Color.ToString().ToUpper();
-                var itemColor = item.Color;
-                // var Brush = new SolidColorBrush();
-                Brush.Color = Color.FromArgb(255,
-                    Convert.ToByte(itemColor.Substring(1, 2), 16),
-                    Convert.ToByte(itemColor.Substring(3, 2), 16),
-                    Convert.ToByte(itemColor.Substring(5, 2), 16));
+            Title.Text = "百草霜";
+            RGB.Text = "RGB:48,48,48";
+            CMYK.Text = "CMYK:0,0,0,81";
+            pinyin.Text = "BAICAOSHUANG";
+                HEX.Text = "HEX:#303030";
+                Brush.Color = Color.FromArgb(255, 48, 48, 48);
                 Hub.Background = Brush;
-            }
-            catch (System.NullReferenceException)
-            {
-                Title.Text = "缃色";
-                RGB.Text = "RGB:240,194,57";
-                CMYK.Text = "CMYK:6,23,90,0";
-                des.Text = "缃色：浅黄色";
-                HEX.Text = "HEX:#F0C239";
-                Brush.Color = Color.FromArgb(255, 240, 194, 57);
-                Hub.Background = Brush;
-            }
-            
-             
-             
         }
         #endregion
+
     }
 }
